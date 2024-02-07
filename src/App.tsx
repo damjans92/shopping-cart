4dac2742c9401e62ebd85bbd5f9a18d345302c08
Damjan Savkovic <damjans92@gmail.com>
2024-02-07 15:15:32 +0100

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
-import Home from './pages/Home'
 import Store from './pages/Store'
 import About from './pages/About'
 import Navbar from './components/Navbar'
 import { ShoppingCartProvider } from './context/ShoppingCartContext'
-import ShoppingCart from './components/ShoppingCart'
 
 function App() {
   return (
